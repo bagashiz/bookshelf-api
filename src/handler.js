d8e39ce44f6c98c7d3aed2dda8ecd9f931d4ed44
bagashiz <work.bitsy@simplelogin.com>
2023-02-04 15:00:00 +0700

    feat: create PUT handler for update a book by id

--- src/handler.js
+++ src/handler.js
@@ -135,8 +135,79 @@ const getBookByIdHandler = (request, h) => {
     return response;
 };
 
+// PUT /books/{bookId} handler
+const editBookByIdHandler = (request, h) => {
+    const { bookId } = request.params;
+
+    const {
+        name,
+        year,
+        author,
+        summary,
+        publisher,
+        pageCount,
+        readPage,
+        reading,
+    } = request.payload;
+
+    const updatedAt = new Date().toISOString();
+
+    const index = books.findIndex((book) => book.id === bookId);
+
+    if (index !== -1) {
+        // client tidak melampirkan properti name pada request body.
+        if (!name) {
+            const response = h.response({
+                status: 'fail',
+                message: 'Gagal memperbarui buku. Mohon isi nama buku',
+            });
+            response.code(400);
+            return response;
+        }
+
+        // client melampirkan nilai properti readPage
+        // yang lebih besar dari nilai properti pageCount.
+        if (readPage > pageCount) {
+            const response = h.response({
+                status: 'fail',
+                message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
+            });
+            response.code(400);
+            return response;
+        }
+
+        books[index] = {
+            ...books[index],
+            name,
+            year,
+            author,
+            summary,
+            publisher,
+            pageCount,
+            readPage,
+            reading,
+            updatedAt,
+        };
+
+        const response = h.response({
+            status: 'success',
+            message: 'Buku berhasil diperbarui',
+        });
+        response.code(200);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Gagal memperbarui buku. Id tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+};
+
 module.exports = {
     addBookHandler,
     getAllBooksHandler,
     getBookByIdHandler,
+    editBookByIdHandler,
 };
